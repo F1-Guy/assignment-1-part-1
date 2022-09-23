de073d0a1a3a39c139eb98c254c756abbf1184a6
F1-Guy <bartol.cagalj@gmail.com>
2022-09-23 10:46:48 +0200

    Initial commit

---
+++ assignment1/FootballPlayer.cs
@@ -0,0 +1,36 @@
+﻿namespace assignment1
+{
+    public class FootballPlayer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public int ShirtNumber { get; set; }
+
+        public void ValidateName()
+        {
+            if (Name.Length < 2)
+            {
+                throw new ArgumentOutOfRangeException("Name cannot be less than 2 characters");
+            }
+        }
+
+        public void ValidateAge()
+        {
+            if (Age < 1)
+            {
+                throw new ArgumentOutOfRangeException("Age cannot be less than 1");
+            }
+        }
+
+        public void ValidateNo()
+        {
+            if (ShirtNumber < 1 || ShirtNumber > 99)
+            {
+                throw new ArgumentOutOfRangeException("Shirt number must be between 1 and 99");
+            }
+        }
+    }
+}
---
+++ assignment1Tests/FootballPlayerTests.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace assignment1.Tests
+{
+    [TestClass()]
+    public class FootballPlayerTests
+    {
+        private readonly FootballPlayer goodPlayer = new() { Id = 1, Name = "Digna", Age = 22, ShirtNumber = 69 };
+
+        [TestMethod()]
+        public void ValidateNameTest()
+        {
+            FootballPlayer noname = new() { Id = 2, Name = "", Age = 50, ShirtNumber = 50 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => noname.ValidateName());
+
+            FootballPlayer shortNamePlayer = new() { Id = 2, Name = "O", Age = 50, ShirtNumber = 50 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => shortNamePlayer.ValidateName());
+
+            FootballPlayer shortNamePlayer2 = new() { Id = 3, Name = "Ba", Age = 50, ShirtNumber = 50 };
+            shortNamePlayer2.ValidateName();
+
+            goodPlayer.ValidateName();
+        }
+
+        [TestMethod()]
+        public void ValidateAgeTest()
+        {
+            FootballPlayer babyPlayer = new() { Id = 4, Name = "Szymon", Age = 0, ShirtNumber = 50 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => babyPlayer.ValidateAge());
+
+            FootballPlayer babyPlayer1 = new() { Id = 5, Name = "Maria", Age = 1, ShirtNumber = 50 };
+            babyPlayer1.ValidateAge();
+
+            goodPlayer.ValidateAge();
+        }
+
+        [TestMethod()]
+        public void ValidateNoTest()
+        {
+            FootballPlayer wrongShirtNumberPlayer = new() { Id = 6, Name = "Szymon", Age = 50, ShirtNumber = -69 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => wrongShirtNumberPlayer.ValidateNo());
+
+            FootballPlayer wrongShirtNumberPlayer2 = new() { Id = 7, Name = "Szymon", Age = 50, ShirtNumber = 2000 };
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => wrongShirtNumberPlayer2.ValidateNo());
+
+            FootballPlayer edgeCasePlayer = new() { Id = 8, Name = "Szymon", Age = 50, ShirtNumber = 1 };
+            edgeCasePlayer.ValidateNo();
+
+            FootballPlayer edgeCasePlayer2 = new() { Id = 9, Name = "Szymon", Age = 50, ShirtNumber = 99 };
+            edgeCasePlayer.ValidateNo();
+
+            goodPlayer.ValidateAge();
+        }
+    }
+}
